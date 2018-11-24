74730f44043533dbb0fbabeef4cb546e7b3384e5
Jarek Magulski <0951724@hr.nl>
2018-11-24 20:21:15 +0100

    kijkDAN!

--- project_c/Startup.cs
+++ project_c/Startup.cs
@@ -22,6 +22,7 @@ namespace project_c
             Configuration = configuration;
         }
 
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
