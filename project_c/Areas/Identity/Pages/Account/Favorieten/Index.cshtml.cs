da75f6b9629ac3782bdc2bc449d82258e27c4027
areopa <36472947+areopa@users.noreply.github.com>
2018-12-08 18:43:37 +0100

    Nearly working lookup of favorites

--- project_c/Areas/Identity/Pages/Account/Favorieten/Index.cshtml.cs
+++ project_c/Areas/Identity/Pages/Account/Favorieten/Index.cshtml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -10,22 +14,51 @@ using project_c.Models;
 
 namespace project_c.Areas.Identity.Pages.Account.Favorieten
 {
-    public class FavorietenModel : PageModel
+    public class IndexModel : PageModel
     {
-        private readonly project_c.Data.ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public FavorietenModel(project_c.Data.ApplicationDbContext context)
+        public IndexModel(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
-        public IList<Models.Favorieten> Favorieten { get;set; }
+
+
+        public IList<Game> GamesIdList { get; set; }
 
         public async Task OnGetAsync()
         {
-            Favorieten = await _context.Favorieten
-                .Include(f => f.ApplicationUser).ToListAsync();
+            List<int> GamesId;
+            var user = await _userManager.GetUserAsync(User);
+            var Favorieten = await _context.Favorieten
+                            .FindAsync(user.Id);
 
+            var GameList = Favorieten.GameList;
+
+
+            GamesId = DeserializeByteToIntList(Favorieten.GameList);
+
+            foreach (var thing in GamesId)
+            {
+                var value = thing;
+                GamesIdList.Add(await _context.Games.FindAsync(value));
+            }
+            
+        }
+
+        public virtual List<int> DeserializeByteToIntList(Byte[] serializedList)
+        {
+            List<int> intList = null;
+            IFormatter formatter = new BinaryFormatter();
+            using (MemoryStream stream = new MemoryStream(serializedList))
+            {
+
+                intList = (formatter.Deserialize(stream) as List<int>);
+            }
+            return intList;
         }
     }
 }
