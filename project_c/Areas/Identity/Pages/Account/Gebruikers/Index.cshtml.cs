50c58a43547cb4577718b399e3798b51db920853
Jarek Magulski <0951724@hr.nl>
2019-01-02 19:39:23 +0100

    Admin sheisse

---
+++ project_c/Areas/Identity/Pages/Account/Gebruikers/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace project_c.Areas.Identity.Pages.Account.Gebruikers
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
