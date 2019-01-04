97b5da2d37b06d445e2c1eedce00ced9b79cc938
areopa <36472947+areopa@users.noreply.github.com>
2019-01-04 21:54:54 +0100

    DummyData Werken

--- project_c/Controllers/AdminController.cs
+++ project_c/Controllers/AdminController.cs
@@ -88,84 +88,162 @@ namespace project_c.Controllers
             }
         }
 
+
         public async Task<IActionResult> AddDummyOrders()
         {
             await AdminCheck();
-
             var user1 = await _userManager.FindByEmailAsync("ongeregistreerd@ongeregistreerd.ongeregistreerd");
             var user2 = await _userManager.FindByEmailAsync("donald@duck.voorbeeld");
             var user3 = await _userManager.FindByEmailAsync("dagobert@duck.voorbeeld");
             var user4 = await _userManager.FindByEmailAsync("lara@croft.voorbeeld");
             var user5 = await _userManager.FindByEmailAsync("doutzen@krous.voorbeeld");
 
-            List<ApplicationUser> UserList = new List<ApplicationUser> { user1, user2, user3, user4, user5};
-            
+            List<ApplicationUser> UserList = new List<ApplicationUser> { user1, user2, user3, user4, user5 };
+
             Random random = new Random();
-            
+
+            var order = new Order { };
 
             foreach (var item in UserList)
             {
-                int randomNumber = random.Next(2, 5);
+                int randomNumber = random.Next(2, 8);
                 for (int i = 0; i < randomNumber; i++)
                 {
-                    AddOrders(item);
+                    order = await AddOrders(item);
+
+                    int randomNumber1 = random.Next(1, 3);
+                    for (int j = 0; j < randomNumber1; j++)
+                    {
+                        await AddGames(order);
+                    }
                 }
             }
+
             return View(nameof(Index));
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async void AddOrders(ApplicationUser user)
+        public async Task<Order> AddOrders(ApplicationUser user)
         {
-
             Random random = new Random();
             int randomNumber = random.Next(1, 1000);
             var randPrice = random.Next(10, 60);
             var endDate = new DateTime(2018, 09, 01);
             var startDate = new DateTime(2019, 01, 04);
-
-            TimeSpan timeSpan = endDate - startDate;
-            TimeSpan newSpan = new TimeSpan(0, random.Next(0, (int)timeSpan.TotalMinutes), 0);
-
-            DateTime newDate = startDate + newSpan;
+            var randMonth = random.Next(9, 13);
+            var randDay = random.Next(1, 30);
 
             Order order = new Order
             {
                 UserId = user.Id,
-                OrderDateTime = newDate,
+                OrderDateTime = new DateTime(2018, randMonth, randDay),
                 OrderMail = user.Email,
                 Price = randPrice
             };
 
-            //order wordt opgeslagen bij de ingelogde gebruiker
             _context.Orders.Add(order);
             await _context.SaveChangesAsync();
-
-            int randomNumber2 = random.Next(1, 3);
-            for (int i = 0; i < randomNumber2; i++)
-            {
-                AddGames(order);
-            }
-            
+            return order;
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async void AddGames(Order order)
+        public async Task AddGames(Order order)
         {
             GameOrder go = new GameOrder();
             Game product = new Game();
 
             Random random = new Random();
-            int randomNumber = random.Next(1, 1000);
+            int? randomNumber = random.Next(1, 1000);
 
-            product = _context.Games.Find(_context.Games.Find(randomNumber));
+            product = await _context.Games
+                .FindAsync(randomNumber);
+                
             go.Order = order;
             go.Game = product;
 
             _context.GameOrder.Add(go);
             await _context.SaveChangesAsync();
         }
+
+
+
+        //public async Task<IActionResult> AddDummyOrders()
+        //{
+        //    await AdminCheck();
+
+        //    var user1 = await _userManager.FindByEmailAsync("ongeregistreerd@ongeregistreerd.ongeregistreerd");
+        //    var user2 = await _userManager.FindByEmailAsync("donald@duck.voorbeeld");
+        //    var user3 = await _userManager.FindByEmailAsync("dagobert@duck.voorbeeld");
+        //    var user4 = await _userManager.FindByEmailAsync("lara@croft.voorbeeld");
+        //    var user5 = await _userManager.FindByEmailAsync("doutzen@krous.voorbeeld");
+
+        //    List<ApplicationUser> UserList = new List<ApplicationUser> { user1, user2, user3, user4, user5 };
+
+        //    Random random = new Random();
+
+
+        //    foreach (var item in UserList)
+        //    {
+        //        int randomNumber = random.Next(1, 3);
+        //        for (int i = 0; i < randomNumber; i++)
+        //        {
+        //            AddOrders(item);
+        //        }
+        //    }
+        //    return View(nameof(Index));
+        //}
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async void AddOrders(ApplicationUser user)
+        //{
+
+        //    Random random = new Random();
+        //    int randomNumber = random.Next(1, 1000);
+        //    var randPrice = random.Next(10, 60);
+        //    var endDate = new DateTime(2018, 09, 01);
+        //    var startDate = new DateTime(2019, 01, 04);
+        //    var randMonth = random.Next(9, 12);
+        //    var randDay = random.Next(1, 29);
+
+        //    Order order = new Order
+        //    {
+        //        UserId = user.Id,
+        //        OrderDateTime = new DateTime(2018, randMonth, randDay),
+        //        OrderMail = user.Email,
+        //        Price = randPrice
+        //    };
+
+        //    order wordt opgeslagen bij de ingelogde gebruiker
+        //    _context.Orders.Add(order);
+        //    await _context.SaveChangesAsync();
+
+        //    int randomNumber2 = random.Next(1, 2);
+        //    for (int i = 0; i < randomNumber2; i++)
+        //    {
+        //        AddGames(order);
+        //    }
+
+        //}
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async void AddGames(Order order)
+        //{
+        //    GameOrder go = new GameOrder();
+        //    Game product = new Game();
+
+        //    Random random = new Random();
+        //    int randomNumber = random.Next(1, 1000);
+
+        //    product = _context.Games.Find(_context.Games.Find(randomNumber));
+        //    go.Order = order;
+        //    go.Game = product;
+
+        //    _context.GameOrder.Add(go);
+        //    await _context.SaveChangesAsync();
+        //}
     }
 }
