34530360f992cd27e2739021fe4295c9b25df6e3
areopa <36472947+areopa@users.noreply.github.com>
2019-01-05 13:13:10 +0100

    Create admin fixed en admin panel verschil admin en superadmin gefixt

--- project_c/Controllers/AdminController.cs
+++ project_c/Controllers/AdminController.cs
@@ -12,6 +12,7 @@ using System.Web;
 using System.Net;
 using System.Net.Mail;
 using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 
 namespace project_c.Controllers
 {
@@ -38,26 +39,75 @@ namespace project_c.Controllers
             await AdminCheck();
             return View();
         }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> CreateAdmin([Bind("UserName, Email, FirstName, FamilyName, BirthDate, AccountType")] ApplicationUser user)
-        {
 
+        [HttpPost]
+        public async Task<IActionResult> CreateAdmin(InputModel Input)
+        {
+            await AdminCheck();
             if (ModelState.IsValid)
             {
-                var result = await _userManager.CreateAsync(user);
-                user.AccountType = "Admin";
-                user.UserName = user.Email;
+                var user = new ApplicationUser {
+                    FirstName = Input.FirstName,
+                    FamilyName = Input.FamilyName,
+                    BirthDate = Input.BirthDate,
+                    UserName = Input.Email,
+                    Email = Input.Email,
+                    AccountCreated = DateTime.Now,
+                    AccountType = "Admin"
+                };
+                var result = await _userManager.CreateAsync(user, Input.Password);
+
                 if (result.Succeeded)
                 {
-                    await _userManager.AddPasswordAsync(user, "Wachtwoord1!");
                     await _userManager.AddToRoleAsync(user, "Admin");
+                    return RedirectToAction("Index", "Home");
                 }
-                _context.SaveChanges();
-                return RedirectToAction("Index");
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
             }
+            return View();
+        }
 
-            return View(user);
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "Voornaam")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "Achternaam")]
+            public string FamilyName { get; set; }
+
+            [Required]
+            [Display(Name = "Geboortedatum")]
+            [DataType(DataType.Date)]
+            public DateTime BirthDate { get; set; }
+
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
         }
 
         [NonAction]
