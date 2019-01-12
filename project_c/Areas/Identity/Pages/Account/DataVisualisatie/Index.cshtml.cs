85cafb6e2abd6b5735577a0d01b8e7d264bd5e31
areopa <36472947+areopa@users.noreply.github.com>
2019-01-13 00:11:01 +0100

    Final Fix

--- project_c/Areas/Identity/Pages/Account/DataVisualisatie/Index.cshtml.cs
+++ project_c/Areas/Identity/Pages/Account/DataVisualisatie/Index.cshtml.cs
@@ -17,7 +17,7 @@ namespace project_c.Areas.Identity.Pages.Account.DataVisualisatie
     public class OrderDateGroup
     {
         [DataType(DataType.Date)]
-        public DateTime? OrderDate { get; set; }
+        public DateTime OrderDate { get; set; }
 
         public int OrderCount { get; set; }
     }
@@ -33,7 +33,10 @@ namespace project_c.Areas.Identity.Pages.Account.DataVisualisatie
             _context = context;
         }
 
-        public IList<OrderDateGroup> Orders { get; set; }
+        public IEnumerable<OrderDateGroup> Orders0 { get; set; }
+        //public IList<OrderDateGroup> Orders1 { get; set; }
+        public IList<DateTime> DateList = new List<DateTime>();
+
         public string[] OrderDates { get; set; }
         public int[] OrderCount { get; set; }
 
@@ -42,33 +45,88 @@ namespace project_c.Areas.Identity.Pages.Account.DataVisualisatie
         {
             IQueryable<OrderDateGroup> data =
                 from orders in _context.Orders
-                group orders by orders.OrderDateTime.Date into dateGroup
+                group orders by new
+                    {
+                        orders.OrderDateTime.Year,
+                        orders.OrderDateTime.Month,
+                        orders.OrderDateTime.Day
+                    } into dateGroup
                 select new OrderDateGroup()
                 {
-                    OrderDate = dateGroup.Key,
+                    OrderDate = new DateTime(dateGroup.Key.Year, dateGroup.Key.Month, dateGroup.Key.Day),
                     OrderCount = dateGroup.Count()
                 };
 
-            Orders = await data.AsNoTracking().ToListAsync();
 
-            var OrderedList = Orders.OrderBy(x => x.OrderDate);
-            var FirstDate = OrderedList.Min(x => x.OrderDate);
-            var LastDate = OrderedList.Max(x => x.OrderDate);
+            DateTime FirstDate = data.Min(x => x.OrderDate);
+            DateTime LastDate = data.Max(x => x.OrderDate);
 
+            await CreateDates(FirstDate, LastDate);
+
+            Orders0 = await data.ToListAsync();
+
+            await CheckDates();
+
+            
 
             List<string> OrderDatesList = new List<string> { };
             List<int> OrderCountList = new List<int> { };
 
-
-            foreach (var item in Orders)
+            foreach (var item in DateList)
             {
-                string dates = item.OrderDate.ToString();
-                int counts = item.OrderCount;
-                OrderDatesList.Add(dates);
-                OrderCountList.Add(counts);
+                var match = Orders0.FirstOrDefault(x => x.OrderDate == item);
+                if (match != null)
+                {
+                    OrderDatesList.Add(item.ToShortDateString());
+                    OrderCountList.Add(match.OrderCount);
+
+                }
+                else
+                {
+                    
+                    OrderDatesList.Add(item.ToShortDateString());
+                    OrderCountList.Add(0);
+                }
             }
+
+            //foreach (var item in Orders0)
+            //{
+            //    OrderDatesList.Add(item.OrderDate.ToShortDateString());
+            //    OrderCountList.Add(item.OrderCount);
+            //}
+
             OrderDates = OrderDatesList.ToArray();
             OrderCount = OrderCountList.ToArray();
+
+            
+            
         }
+
+        public async Task CreateDates(DateTime FirstDate, DateTime LastDate)
+        {    
+            for (var dt = FirstDate; dt <= LastDate; dt = dt.AddDays(1))
+            {
+                DateList.Add(dt);
+                Console.WriteLine(dt);
+            }
+        }
+
+        public async Task CheckDates()
+        {
+            foreach (var item in DateList)
+            {
+                var match = Orders0.FirstOrDefault(x => x.OrderDate == item);
+                if (match == null)
+                {
+
+                    Orders0.Append(new OrderDateGroup()
+                    {
+                        OrderDate = item,
+                        OrderCount = 0
+                    });
+                }
+            }
+        }
+
     }
 }
