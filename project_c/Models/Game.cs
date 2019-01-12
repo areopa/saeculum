1943354b97587a5a9684ede22156ac6a94f04fb1
areopa <36472947+areopa@users.noreply.github.com>
2019-01-13 00:24:04 +0100

    taalerrors gefixt

--- project_c/Models/Game.cs
+++ project_c/Models/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,16 +13,22 @@ namespace project_c.Models
         //PK
         public int Id { get; set; }
         //titel van de Game
+        [Required]
         public string Title { get; set; }
         //Genre van de Game
+        [Required]
         public string Genre { get; set; }
         //Pegi leeftijd van de Game
+        [Required]
         public int Pegi { get; set; }
         //Beschrijvende tekst van de Game
+        [Required]
         public string Description { get; set; }
         //Prijs van de Game
+        [Required]
         public decimal Price { get; set; }
         //Bedrijf dat de Game geproduceerd heeft
+        [Required]
         public string ProducingCompany { get; set; }
         //collectie van orders die horen bij een Game
         public virtual List<GameOrder> Orders { get; set; }
